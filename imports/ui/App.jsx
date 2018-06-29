61a8a78f0e087861c5f037a7bf15b1de71bfbad6
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-29 11:24:36 -0500

    Body skeleton

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 
+// Our components
+import TeamList from './Team-List';
+import TeamStats from './Team-Stats';
+import Player from './Player';
+
 export default class App extends Component {
   render(){
     return (
       <MuiThemeProvider>
-        <RaisedButton label="Oh Yeah we did it!" />
+        <div className="container">
+          <div className="row">
+            <div className="col s12 m7"><Player /></div>
+            <div className="col s12 m5"><TeamStats /></div>
+            <div className="col s12 m5"><TeamList /></div>
+          </div>
+        </div>
       </MuiThemeProvider>
     )
   }
