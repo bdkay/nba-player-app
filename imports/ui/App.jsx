307ee922c5449b3e1d16e06c52ae5d0f20f2dbb0
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-29 16:38:24 -0500

    Functioning sandbox server

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import { List } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 
+// Contain the data from the db, pass it onto the components
+import { createContainer } from 'meteor/react-meteor-data';
+import { Link } from 'react-router-dom';
+
+// database
+import { Players } from  '../api/players';
+
 // Our components
 import TeamList from './Team-List';
 import TeamStats from './Team-Stats';
 import Player from './Player';
 
-export default class App extends Component {
+class App extends Component {
 
   // Use a constructor to set up state
   constructor(props){
@@ -19,63 +27,12 @@ export default class App extends Component {
     this.state = { players: [] };
   }
 
-   // ...and then set the new state with the following array of players
-
-  componentWillMount(){
-    this.setState({ players: [
-      {
-        _id: 1,
-        name: "Kris Dunn",
-        overall: 72,
-        per: 7.8,
-        ts: 43,
-        ast: 21,
-        usg: 15
-      },
-      {
-        _id: 2,
-        name: "Justin Holiday",
-        overall: 74,
-        per: 12.7,
-        ts: 55,
-        ast: 9,
-        usg: 17
-      },
-      {
-        _id: 3,
-        name: "Robin Lopez",
-        overall: 76,
-        per: 14.8,
-        ts: 51,
-        ast: 5,
-        usg: 16
-      },
-      {
-        _id: 4,
-        name: "Nikola Mirotic",
-        overall: 76,
-        per: 12,
-        ts: 51,
-        ast: 6,
-        usg: 19
-      },
-      {
-        _id: 5,
-        name: "Denzel Valentine",
-        overall: 68,
-        per: 6.2,
-        ts: 45,
-        ast: 8,
-        usg: 16
-      }
-    ]});
-  }
-
   // Get players and map them to the component
   // Pass the entire player object so team-list has access to all its props
 
   renderPlayers(){
-    return this.state.players.map((player) => (
+    // Passing players as props instead of state now (from the container)
+    return this.props.players.map((player) => (
       <TeamList key={player._id} player={player} />
     ));
   }
@@ -96,7 +53,7 @@ export default class App extends Component {
           <div className="row">
             <div className="col s12 m7"><Player /></div>
             <div className="col s12 m5">
-              <h2>Team List</h2>
+              <h2>Team List</h2><Link to="/new" className="btn waves-effect waves-light">Add Player</Link>
               <Divider />
               <List>
                 { this.renderPlayers() }
@@ -110,3 +67,20 @@ export default class App extends Component {
     )
   }
 }
+
+// Check what proptypes are being passed to the db, ensure it's an array
+App.propTypes = {
+  players: PropTypes.array.isRequired
+};
+
+// Create a container of data
+export default createContainer(() => {
+
+  // Subscribe to the collection published from the server, this is how we get data from Meteor with React
+  Meteor.subscribe('players');
+
+  return {
+    players: Players.find({}, { sort: { name: 1 }}).fetch()
+  };
+
+}, App);
