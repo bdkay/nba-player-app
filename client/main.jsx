363e26a31ce2d059c2213c60a889e490de947282
Brendon Kuleck <brendonkuleck@gmail.com>
2018-06-29 14:04:10 -0500

    npm to yarn

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import App from '../imports/ui/App';
+import Example from '../imports/ui/Example';
+import Lost from '../imports/ui/Lost';
 
 injectTapEventPlugin();
 
