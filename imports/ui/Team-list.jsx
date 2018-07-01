1fa275c0258e1d15cacc5b69bce82e769e6e830c
Brendon Kuleck <brendonkuleck@gmail.com>
2018-07-01 00:24:02 -0500

    One more function

--- imports/ui/Team-list.jsx
+++ imports/ui/Team-list.jsx
@@ -3,12 +3,19 @@ import Avatar from 'material-ui/Avatar';
 import { ListItem } from 'material-ui/List';
 
 export default class TeamList extends Component {
+
+  updateCurrentPlayer(player){
+    this.setState({
+      currentPlayer: player
+    });
+  }
+  
   render(){
     return (
       <ListItem
         primaryText={this.props.player.name}
         leftAvatar={<Avatar src="player.jpg" />}
-        onClick={this.updateCurrentPlayer}
+        onClick={this.updateCurrentPlayer.bind(this, this.props.player)}
       />
     )
   }
